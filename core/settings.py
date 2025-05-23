a1d4b4d022bb4db7f51656ea8a7b91d56e5ac3ae
nurai002 <nurajtemirbaeva15@gmail.com>
2025-05-23 17:37:20 +0600

    fix

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['nportfolio.webtm.ru', 'localhost']
+ALLOWED_HOSTS = ['nportfolio.webtm.ru', 'localhost', '127.0.0.1']
 
 # Application definition
 
